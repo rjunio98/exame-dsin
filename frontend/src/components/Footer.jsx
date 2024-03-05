d81da110147560bdb0de3740cd63fe5d2195eaf2
rjunio98 <robledorjunior1@gmail.com>
2024-03-04 22:09:42 -0300

    Adiciona componente Footer ao projeto

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="fixed bottom-0 left-0 w-full bg-rose-100 p-4 text-center">
+        <p className="text-xl">&copy; Cabeleireira Leila Salão de Beleza</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
