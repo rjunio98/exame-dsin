0f8d3bf7880714b9ce6948eb91fc09f5bc134db5
rjunio98 <robledorjunior1@gmail.com>
2024-03-05 19:05:28 -0300

    Atualizar texto do rodapé

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div>
       <footer className="fixed bottom-0 left-0 w-full bg-rose-100 p-4 text-center">
-        <p className="text-xl">&copy; Cabeleireira Leila Salão de Beleza</p>
+        <p className="text-xl">&copy; Leila Salão de Beleza</p>
       </footer>
     </div>
   );
