dbad573b382f0ea2b31479eb7c655ccccb2b33be
rjunio98 <robledorjunior1@gmail.com>
2024-03-04 22:09:49 -0300

    Adiciona componente App.jsx com Header, Hero e Footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Hero />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
