bc5fedf95f19e386b780ec60529348e053c93309
rjunio98 <robledorjunior1@gmail.com>
2024-03-05 18:34:38 -0300

    Adiciona componente Modificar para rota /modificar/:id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Agendamento from "./components/Agendamento.jsx";
+import Modificar from "./components/Modificar.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/agendamento",
     element: <Agendamento />,
+  },
+  {
+    path: "/modificar/:id",
+    element: <Modificar />,
   },
 ]);
 
