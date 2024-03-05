be66e82f7187e7dc286a4ee21987528690ae800a
rjunio98 <robledorjunior1@gmail.com>
2024-03-04 22:09:45 -0300

    Adiciona componente de Agendamento

---
+++ frontend/src/components/Agendamento.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import api from "../api/api";
+
+function Agendamento() {
+  return (
+    <div>
+      <h1>fdfdfdfd</h1>
+    </div>
+  );
+}
+
+export default Agendamento;
